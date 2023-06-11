b88ce46ba6c298351aaeb08c8f86fd0401b2dcb4
Mikalai Seva <mikalai.seva@gmail.com>
2023-06-11 15:06:59 +0200

    Add mix.exs with dependencies listed

---
+++ mix.exs
@@ -0,0 +1,46 @@
+defmodule AirDB.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :air_db,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {AirDB.Application, []}
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:postgrex, ">= 0.0.0"},
+      {:ecto_sql, "~> 3.10"},
+      {:nimble_csv, "~> 1.2"},
+      {:jason, "~> 1.4"},
+      {:stream_data, "~> 0.5", only: [:dev, :test]},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
+    ]
+  end
+end
