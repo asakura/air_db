3d135e678d548454177cf8e51b1816051ffb2e8c
Mikalai Seva <mikalai.seva@gmail.com>
2023-06-14 08:08:50 +0200

    Add Aircraft schema module

---
+++ lib/air_db/aircraft.ex
@@ -0,0 +1,28 @@
+defmodule AirDB.Aircraft do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias AirDB.Seat
+
+  @primary_key false
+  schema "aircrafts" do
+    field :aircraft_code, :string, primary_key: true
+    field :model, :string
+    field :range, :integer
+
+    has_many :seats, Seat,
+      foreign_key: :aircraft_code,
+      references: :aircraft_code,
+      on_replace: :mark_as_invalid
+  end
+
+  @doc false
+  def changeset(aircraft, params \\ %{}) do
+    aircraft
+    |> cast(params, [:aircraft_code, :model, :range])
+    |> cast_assoc(:seats, with: &Seat.changeset/2)
+    |> validate_required([:aircraft_code, :model, :range])
+    |> validate_length(:aircraft_code, is: 3, count: :bytes)
+    |> unique_constraint(:aircraft_code, name: :aircrafts_pkey)
+    |> check_constraint(:range, name: :aircrafts_range_check, message: "should be greater than 0")
+  end
+end
