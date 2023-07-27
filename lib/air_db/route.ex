e5c6d4cf7e7e472fc73464a164d9719d3fc37862
Mikalai Seva <mikalai.seva@gmail.com>
2023-07-27 08:04:02 +0200

    add a number of relationship to Route schema

--- lib/air_db/route.ex
+++ lib/air_db/route.ex
@@ -4,14 +4,26 @@ defmodule AirDB.Route do
   @primary_key false
   schema "routes" do
     field :flight_no, :string
-    field :departure_airport_code, :string
     field :departure_airport_name, :string
     field :departure_city, :string
-    field :arrival_airport_code, :string
     field :arrival_airport_name, :string
     field :arrival_city, :string
-    field :aircraft_code, :string
     field :duration, :binary
     field :days_of_week, {:array, :integer}
+
+    belongs_to :aircraft, AirDB.Aircraft,
+      foreign_key: :aircraft_code,
+      references: :aircraft_code,
+      type: :string
+
+    belongs_to :departure_airport, AirDB.Airport,
+      foreign_key: :departure_airport_code,
+      references: :airport_code,
+      type: :string
+
+    belongs_to :arrival_airport, AirDB.Airport,
+      foreign_key: :arrival_airport_code,
+      references: :airport_code,
+      type: :string
   end
 end
