966c32e3246128eba1411c1a2cfb21a4f8e6bfc9
Mikalai Seva <mikalai.seva@gmail.com>
2023-08-01 08:43:32 +0200

    Add relation defs to FlightExtended schema

--- lib/air_db/flight_extended.ex
+++ lib/air_db/flight_extended.ex
@@ -11,19 +11,31 @@ defmodule AirDB.FlightExtended do
     field :scheduled_arrival_local, :naive_datetime
     # interval
     field :scheduled_duration, :binary
-    field :departure_airport_code, :string
     field :departure_airport_name, :string
     field :departure_city, :string
-    field :arrival_airport_code, :string
     field :arrival_airport_name, :string
     field :arrival_city, :string
     field :status, :string
-    field :aircraft_code, :string
     field :actual_departure, :utc_datetime
     field :actual_departure_local, :naive_datetime
     field :actual_arrival, :utc_datetime
     field :actual_arrival_local, :naive_datetime
     # interval
     field :actual_duration, :binary
+
+    belongs_to :departure_airport, Airport,
+      foreign_key: :departure_airport_code,
+      references: :airport_code,
+      type: :string
+
+    belongs_to :arrival_airport, Airport,
+      foreign_key: :arrival_airport_code,
+      references: :airport_code,
+      type: :string
+
+    belongs_to :aircraft, Aircraft,
+      foreign_key: :aircraft_code,
+      references: :aircraft_code,
+      type: :string
   end
 end
